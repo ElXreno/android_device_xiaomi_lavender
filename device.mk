8f0b447930353a62debb7bee0d3be25d4ecb26f8
ElXreno <elxreno@gmail.com>
2020-05-13 20:05:08 +0300

    lavender: Include Longshot

--- device.mk
+++ device.mk
@@ -27,6 +27,9 @@ $(call inherit-product, device/xiaomi/sdm660-common/sdm660.mk)
 # GoogleCamera
 $(call inherit-product, packages/apps/GoogleCamera/gcam.mk)
 
+# Longshot
+$(call inherit-product, packages/apps/Longshot/longshot.mk)
+
 # Device Path
 DEVICE_PATH := device/xiaomi/lavender
 
