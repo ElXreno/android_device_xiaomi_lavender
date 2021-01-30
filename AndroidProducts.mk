abc587b05e94f8d35573f38dd9dface9eabea773
ElXreno <elxreno@gmail.com>
2021-01-30 14:46:40 +0300

    Fluid --> LineageOS
    Signed-off-by: ElXreno <elxreno@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/fluid_lavender.mk
+    $(LOCAL_DIR)/lineage_lavender.mk
 
 COMMON_LUNCH_CHOICES := \
-    fluid_lavender-user \
-    fluid_lavender-userdebug \
-    fluid_lavender-eng
+    lineage_lavender-user \
+    lineage_lavender-userdebug \
+    lineage_lavender-eng
